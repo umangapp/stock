8d05b40dc442c61e8f697ae7571ae46d8e28f024
umangapp <accumang@gmail.com>
2026-04-21 17:46:37 +0700

    Create middleware.ts

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  const { data: { session } } = await supabase.auth.getSession()
+
+  // ถ้าพยายามเข้าหน้าสแกน หรือ แอดมิน แต่ยังไม่ล็อคอิน
+  if (!session && (req.nextUrl.pathname.startsWith('/scan') || req.nextUrl.pathname.startsWith('/admin'))) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  return res
+}
+
+export const config = {
+  matcher: ['/scan/:path*', '/admin/:path*'],
+}
